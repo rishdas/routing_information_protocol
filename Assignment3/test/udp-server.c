5ff8a882121e3e1f60fd705e6cb20c20b1c5b833
Lucas Brasilino <lbrasili@github.iu.edu>
2015-11-11 11:50:00 -0500

    modifying udp-server to test update message receiving and parsing

--- Assignment3/test/udp-server.c
+++ Assignment3/test/udp-server.c
@@ -1,34 +1,35 @@
-
-/* Sample UDP server */
+/* UDP receiving message test */
 
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
 
+#include <rip_main.h>
+
 int main(int argc, char**argv)
 {
-   int sockfd,n;
+    int sockfd,n,i,recv_number;
    struct sockaddr_in servaddr,cliaddr;
    socklen_t len;
-   char mesg[1000];
 
    sockfd=socket(AF_INET,SOCK_DGRAM,0);
 
-   bzero(&servaddr,sizeof(servaddr));
+   memset(&servaddr,0,sizeof(servaddr));
    servaddr.sin_family = AF_INET;
    servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
-   servaddr.sin_port=htons(32000);
+   servaddr.sin_port=htons(8080);
    bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
 
    for (;;)
    {
-      len = sizeof(cliaddr);
-      n = recvfrom(sockfd,mesg,1000,0,(struct sockaddr *)&cliaddr,&len);
-      sendto(sockfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
-      printf("-------------------------------------------------------\n");
-      mesg[n] = 0;
-      printf("Received the following:\n");
-      printf("%s",mesg);
-      printf("-------------------------------------------------------\n");
+       len = sizeof(cliaddr);
+       n = recvfrom(sockfd,message, MAXROUTE * message_entry_t_len, 0,
+		    (struct sockaddr *)&cliaddr,&len);
+       recv_number = n / message_entry_t_len;
+       for (i = 0; i < recv_number; i++) {
+	   printf ("IP = %s Cost = %d\n", 
+		   rip_net_inet_ntop (message[i].dest_addr), message[i].cost);
+       }
    }
 }
+
