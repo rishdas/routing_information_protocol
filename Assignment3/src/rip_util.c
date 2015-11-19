21ebc92131a94a5e3d1c961092acc73376a5c15a
rishdas <sirshakdas@gmail.com>
2015-11-18 21:27:47 -0500

    Fixing the output:Cosmetic

--- Assignment3/src/rip_util.c
+++ Assignment3/src/rip_util.c
@@ -6,13 +6,16 @@ void rip_util_print_routing_table (void)
     int i;
     char buff[INET_ADDRSTRLEN];
     route_entry_t entry;
- 
-    fprintf (stdout,"Initial routing table:\n"
-	     "Node\tNext Hop\tCost\tTTL\n");
+
+    fprintf(stdout, "Initial routing table:\n");
+    /* fprintf (stdout,"Initial routing table:\n" */
+    /* 	     "Node\tNext Hop\tCost\tTTL\n"); */
+    fprintf (stdout,
+	     "%-26s %-26s %-5s %-5s\n", "Node", "Next Hop", "Cost", "TTL");
     for (i = 0; i < rip_routing_table_entry_number; i++){
 	memset (buff,0,INET_ADDRSTRLEN);
 	entry = routingtable[i];
-	fprintf (stdout,"%s\t%s\t%d\t%d\n", entry->destination->name,
+	fprintf (stdout,"%-26s %-26s %-5d %-5d\n", entry->destination->name,
 		 (entry->nexthop) ? 
 		 entry->nexthop->name : "Null        ", entry->cost, 
 		 entry->ttl);	
